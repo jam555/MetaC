f4f60d3f87ebae277f81f1cc56c1b4eef5d62a02
Jared Maddox <absinthdraco@gmail.com>
2022-10-30 05:01:09 -0500

    Prepping for first upload.
    Signed-off-by: Jared Maddox <absinthdraco@gmail.com>

--- input/lexalike.h
+++ input/lexalike.h
@@ -1,6 +1,26 @@
 #ifndef METAC_INPUT_LEXALIKE_H
  #define METAC_INPUT_LEXALIKE_H
 	
+/*
+MetaC Preprocessor - A macro processor for preprocessing usage.
+Copyright (C) 2022 Jared A. Maddox
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of version 2 of the GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the:
+	Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330,
+	Boston, MA 02111-1307 USA
+*/
+	
 	/* This file started as lexalike.c. It's been modified some, but some of */
 	/*  these functions might not belong here, and other definitely DON'T */
 	/*  belong here. */
--- newPreproc/macro.h
+++ newPreproc/macro.h
@@ -1,6 +1,26 @@
 #ifndef ???
  #define ???
 	
+/*
+MetaC Preprocessor - A macro processor for preprocessing usage.
+Copyright (C) 2022 Jared A. Maddox
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of version 2 of the GNU General Public License as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the:
+	Free Software Foundation, Inc.,
+	59 Temple Place, Suite 330,
+	Boston, MA 02111-1307 USA
+*/
+	
 	/* These are used during macro execution to encode WHAT will be */
 	/*  emitted (or emitting) to the next stage of processing when the */
 	/*  individual structure instance is reached. */
