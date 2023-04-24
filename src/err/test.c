f01e86e02579e1a1414e51ee16b560c472386df3
Jam555 <3349478+jam555@users.noreply.github.com>
2023-04-24 06:31:43 -0500

    Save the err/test.c file for posterity: preprocessing & then compiling this compensated for inadequate GCC error messages...
    Signed-off-by: Jam555 <3349478+jam555@users.noreply.github.com>

---
+++ src/err/test.c
@@ -0,0 +1,8 @@
+#include "../../external_dependencies/libandria4/basic/pascalarray.h"
+
+	/* For the sake of abbreviated names. */
+#include "../../external_dependencies/libandria4/stdabbrevs/metaCabbrevs.h"
+
+
+LIB4_DEFINE_PASCALARRAY_STDDEFINE( chartype_, char );
+LIBANDRIA4_DEFINE_PASCALARRAY_STATICBUILD( outername, name, chartype_, char, 'h', 'e', 'l', 'l', 'o', '\0' );
